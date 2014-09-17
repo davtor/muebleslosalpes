15b38c970bcf1d71757327b46f3b7f6cd34718af
azulsebas123@gmail.com <azulsebas123@gmail.com@e97445f5-1b25-9815-e3fd-e2787d55b712>
2014-09-17 19:44:47 +0000

    cambios en nombre de archivos

---
+++ trunk/mueblesAlpes/src/java/com/mueblesAlpes/Beans/ClienteBean.java
@@ -0,0 +1,118 @@
+package com.mueblesAlpes.Beans;
+
+/**
+ *
+ * @author Administrador
+ */
+
+
+public class ClienteBean {
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private int idTipoDocumento;
+    private int numeroDocumento;
+    private int numeroCelular;
+    private String email;
+    private String fechaNacimiento;
+    private String contrasena;
+
+    public ClienteBean(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, int idTipoDocumento, int numeroDocumento, int numeroCelular, String email, String fechaNacimiento, String contrasena) {
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.idTipoDocumento = idTipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.numeroCelular = numeroCelular;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+        this.contrasena = contrasena;
+    }
+
+    public ClienteBean() {
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public int getIdTipoDocumento() {
+        return idTipoDocumento;
+    }
+
+    public void setIdTipoDocumento(int idTipoDocumento) {
+        this.idTipoDocumento = idTipoDocumento;
+    }
+
+    public int getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(int numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(int numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
---
+++ trunk/mueblesAlpes/src/java/com/mueblesAlpes/Servlets/CreacionClientesServlet.java
@@ -0,0 +1,90 @@
+package com.mueblesAlpes.Servlets;
+
+import com.mueblesAlpes.Beans.ClienteBean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Administrador
+ */
+@WebServlet(name = "creacionClientesServlet", urlPatterns = {"/creacionClientesServlet"})
+public class CreacionClientesServlet extends HttpServlet {
+
+  private ClienteBean cliente = new ClienteBean();
+
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+       String mensaje = "Se creo con exito el cliente:";
+      response.sendRedirect("vistas/creacionClientes.jsp?respuesta=si&cliente=" + cliente.getPrimerNombre()+"&mensaje="+mensaje);
+    } finally {
+      out.close();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    cliente.setPrimerNombre(request.getParameter("primerNombre"));
+    cliente.setSegundoNombre(request.getParameter("segundoNombre"));
+    cliente.setPrimerApellido(request.getParameter("primerApellido"));
+    cliente.setSegundoApellido(request.getParameter("segundoApellido"));
+    cliente.setIdTipoDocumento(Integer.parseInt(request.getParameter("idTipoDocumento")));
+    cliente.setNumeroDocumento(Integer.parseInt(request.getParameter("numeroDocumento")));
+    cliente.setNumeroCelular(Integer.parseInt(request.getParameter("numeroCelular")));
+    cliente.setEmail(request.getParameter("email"));
+    cliente.setFechaNacimiento(request.getParameter("fechaNacimiento"));
+    cliente.setContrasena(request.getParameter("contrasena"));
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+
+}
